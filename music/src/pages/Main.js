49e19fc391476cee57512534cff1718f9e952c53
Juan <juan_oller@yahoo.com>
2022-05-16 14:28:51 -0500

    added some style

--- music/src/pages/Main.js
+++ music/src/pages/Main.js
@@ -6,10 +6,13 @@ const Main = (props) => {
     return (
         <div>
         <h2> hello</h2>
-        <div style={{ backgroundColor: "#44014C", width: "300px", minHeight: "200px"}}>
-      <h2 style={{ padding: "10px 20px", textAlign: "center", color: "white"}}>ToDo</h2>
+        <div style={{ backgroundColor: "#4D8581", width: "98%", maxHeight: "690px"}}>
+      <h2 style={{ padding: "10px 20px"}}></h2>
+     
+      <div style={{ backgroundColor: "#ABDED7", width: "20%", minHeight: "590px",}}>
+      <h3 style={{ padding: "10px 20px"}}></h3>
       </div>
-
+</div>
         </div>
         
     )
