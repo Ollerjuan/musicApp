043fdd95b2c3b82f15fc17a09205715b1f98dcb8
Juan <juan_oller@yahoo.com>
2022-05-19 00:13:49 -0500

    async function not working

--- music/src/pages/Login.js
+++ music/src/pages/Login.js
@@ -1,29 +1,34 @@
 import React, { useEffect, useState } from "react";
 
+
 const Login = () => {
         const CLIENT_ID= "14b01a092c544820bcdf006502843b5e"
         const redirect_uri = "http://localhost:3000"
         const authorization_endPoint = "https://accounts.spotify.com/authorize"
         const response_type = "token"
-        const [token, setToken] = useState ('')
+        // const [token, setToken] = useState("")
 
-        useEffect (() => {
-            // hash is given to us after we have enterd the login info in the url 
-            const hash = window.location.hash   
-            // split and store in local storage
-            const getToken = hash.substring(1).split("&")[0].split("=")[1]
-            window.localStorage.setItem('token', getToken)
-            setToken(getToken)
-    }, [])
+
+        // const getArtist = async (token) => {
+        //     let res = await axios.use("https://api.spotify.com/v1/search")
+            
+        //     let {data} = res.data.use(async function (config){
+        //         config.headers.Authorization = 'Bearer' + token  ;
+        //     },
+        //     this.setArtist.items({data})
     
-    return (
-        // link to and from authorization route 
+        // }
+      
+       
+
+        
+        return (
+            // link to and from authorization route 
             <div className="Login">
                     <a id="login" href={`${authorization_endPoint}?client_id=${CLIENT_ID}&redirect_uri=${redirect_uri}&response_type=${response_type}`}> Click here to Login </a>
       
             </div>
     )
-
-}
     
+}
 export default Login;
--- music/src/pages/Main.js
+++ music/src/pages/Main.js
@@ -1,44 +1,51 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios"
 import Login from "./Login";
+import { useParams } from "react-router";
 
 const Main = () => { 
-    const url = `https://api.spotify.com/v1/search`
+    const [searchKey, setSearchKey] = useState('')
+    const [artists, setArtist] = useState('')
     const [token, setToken] = useState('')
-    let urlParams = new URLSearchParams(window.location.hash.replace(''))
 
+     
 
-//     useEffect (() => {
-//         // hash is given to us after we have enterd the login info in the url 
+    useEffect (() => {
     
-//         const hash = window.location.hash   
-//         console.log(hash.split("&"))
-// }, [])
-        // let url = window.location
-        // let access_token = new URLSearchParams(url.search).get('access_token')
-        
-        //     window.location.hash = ""
-        //     window.localStorage.setItem('token', token)
-        // setToken(token)
+        // hash is given to us after we have enterd the login info in the url 
+        const hash = window.location.hash   
+        // split and store in local storage
+
+        if(token !== '' & hash){
+        const _token = hash.split("&")[0].split("=")[1]
+        window.localStorage.setItem('token', _token)
+        setToken(_token)
+        }
+    }, [])
+
+
+    const getArtist = async (token) => {
+        let res = await axios.use("https://api.spotify.com/v1/search")
+        let {data} = res.data
+        setArtist.item.use
+            headers.searchKey.Authorization = 'Bearer + token';
+        }
+        setArtist.items(data.artists)
+
     
-
-
-
-        
-        
-        
+    console.log(getArtist)
 
         
-
         
-    return(
-        <div className="Main">
+        return(
+            <div className="Main">
             <Login />
-            <form>
-                <input type= "text"/>
+            <form onSubmit={getArtist}>
+                <input type= "text" onchange={(e) => setSearchKey((e).target.value)}/>
                     <button id="search" type="submit"> Search </button>
               
             </form>
+       
        </div>
     )
 }
@@ -50,7 +57,7 @@ export default Main;
 // export function backgroundColor() {
 //     return (
 //         <div>
-//                 <div style={{ backgroundColor: "#4D8581", width: "98%", maxHeight: "690px"}}>
+                // <div style={{ backgroundColor: "#4D8581", width: "98%", maxHeight: "690px"}}>
 //                 <h2 style={{ padding: "10px 20px"}}></h2>
                 
 //                 <div style={{ backgroundColor: "#ABDED7", width: "20%", minHeight: "590px",}}>
